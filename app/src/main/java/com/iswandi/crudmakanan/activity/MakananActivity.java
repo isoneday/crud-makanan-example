1c78891abb7906a6297aa54e707f5f712f0331dc
iswandisaputra <iswandi@imastudio.co.id>
2018-03-13 08:48:28 +0700

    Initial commit crud makanan (mysql)

---
+++ app/src/main/java/com/iswandi/crudmakanan/activity/MakananActivity.java
@@ -0,0 +1,31 @@
+package com.iswandi.crudmakanan.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.iswandi.crudmakanan.R;
+
+public class MakananActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makanan);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
